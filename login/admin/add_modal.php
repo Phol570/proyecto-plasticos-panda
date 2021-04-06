51678e74dabc99ca1ce7d96ee4c4db6d6f1ff3b5
Phol570 <phocamposv@uch.pe>
2021-04-05 23:43:53 -0500

    Cargando archivos

---
+++ login/admin/add_modal.php
@@ -0,0 +1,159 @@
+<!-- Add Product -->
+    <div class="modal fade" id="addproduct" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <center><h4 class="modal-title" id="myModalLabel">Agregar Producto</h4></center>
+                </div>
+                <div class="modal-body">
+				<div class="container-fluid">
+                    <form role="form" method="POST" action="addproduct.php" enctype="multipart/form-data">
+						<div class="container-fluid">
+						<div style="height:15px;"></div>
+						<div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Nombre Producto:</span>
+                            <input type="text" style="width:400px; text-transform:capitalize;" class="form-control" name="name" required>
+                        </div>
+						<div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Categoria:</span>
+                            <select style="width:400px;" class="form-control" name="category">
+								<?php
+									$cat=mysqli_query($conn,"select * from category");
+									while($catrow=mysqli_fetch_array($cat)){
+										?>
+											<option value="<?php echo $catrow['categoryid']; ?>"><?php echo $catrow['category_name']; ?></option>
+										<?php
+									}
+								?>
+							</select>
+                        </div>
+						<div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Proveedor:</span>
+                            <select style="width:400px;" class="form-control" name="supplier">
+								<?php
+									$sup=mysqli_query($conn,"select * from supplier");
+									while($suprow=mysqli_fetch_array($sup)){
+										?>
+											<option value="<?php echo $suprow['userid']; ?>"><?php echo $suprow['company_name']; ?></option>
+										<?php
+									}
+								?>
+							</select>
+                        </div>
+                        <div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Precio:</span>
+                            <input type="text" style="width:400px;" class="form-control" name="price" required>
+                        </div>
+						<div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Cantidad:</span>
+                            <input type="text" style="width:400px;" class="form-control" name="qty">
+                        </div>
+						<div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Foto:</span>
+                            <input type="file" style="width:400px;" class="form-control" name="image">
+                        </div>            
+						</div>
+				</div>
+				</div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Cancelar</button>
+                    <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Registrar</button>
+					</form>
+                </div>
+			</div>
+		</div>
+    </div>
+<!-- /.modal -->
+
+<!-- Add Customer -->
+    <div class="modal fade" id="addcustomer" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <center><h4 class="modal-title" id="myModalLabel">Agregar Cliente</h4></center>
+                </div>
+                <div class="modal-body">
+				<div class="container-fluid">
+                    <form role="form" method="POST" action="addcustomer.php" enctype="multipart/form-data">
+						<div class="container-fluid">
+						<div style="height:15px;"></div>
+						<div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Nombre y Apellido:</span>
+                            <input type="text" style="width:400px; text-transform:capitalize;" class="form-control" name="name" required>
+                        </div>
+						<div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Dirección:</span>
+                            <input type="text" style="width:400px; text-transform:capitalize;" class="form-control" name="address">
+                        </div>
+						<div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Celular:</span>
+                            <input type="text" style="width:400px;" class="form-control" name="contact">
+                        </div>
+						<div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Usuario:</span>
+                            <input type="text" style="width:400px;" class="form-control" name="username" required>
+                        </div>
+						<div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Contraseña:</span>
+                            <input type="password" style="width:400px;" class="form-control" name="password" required>
+                        </div>  						
+						</div>
+				</div>
+				</div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Cancelar</button>
+                    <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Registrar</button>
+					</form>
+                </div>
+			</div>
+		</div>
+    </div>
+<!-- /.modal -->
+
+<!-- Add Supplier -->
+    <div class="modal fade" id="addsupplier" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <center><h4 class="modal-title" id="myModalLabel">Add New Supplier</h4></center>
+                </div>
+                <div class="modal-body">
+				<div class="container-fluid">
+                    <form role="form" method="POST" action="addsupplier.php" enctype="multipart/form-data">
+						<div class="container-fluid">
+						<div style="height:15px;"></div>
+						<div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Company:</span>
+                            <input type="text" style="width:400px; text-transform:capitalize;" class="form-control" name="name" required>
+                        </div>
+						<div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Address:</span>
+                            <input type="text" style="width:400px; text-transform:capitalize;" class="form-control" name="address">
+                        </div>
+						<div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Contact Info:</span>
+                            <input type="text" style="width:400px;" class="form-control" name="contact">
+                        </div>
+						<div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Username:</span>
+                            <input type="text" style="width:400px;" class="form-control" name="username" required>
+                        </div>
+						<div class="form-group input-group">
+                            <span style="width:120px;" class="input-group-addon">Password:</span>
+                            <input type="password" style="width:400px;" class="form-control" name="password" required>
+                        </div>  						
+						</div>
+				</div>
+				</div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Cancel</button>
+                    <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Save</button>
+					</form>
+                </div>
+			</div>
+		</div>
+    </div>
+<!-- /.modal -->
---
+++ login/conn.php
@@ -0,0 +1,8 @@
+<?php
+
+$conn = mysqli_connect("localhost","id15781835_pruebauch2","bGA5sDfG=><kvz\-","id15781835_pruebauch1");
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+ 
+?>
